e64a3e8c98d8c8f6ea4f325ffbd0870b27659dc2
vipuliitm <118936812+vipuliitm@users.noreply.github.com>
2022-11-24 03:03:25 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.streamlt/
+
+echo"\
+[general]\n\
+email = \"21f1005862@student.onlinedegree.iitm.ac.in"\n\
+"> ~/.streamlit/credntials.toml
+
+port = $PORT\n\
+"> ~/.streamlit/config.toml
