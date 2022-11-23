5062a18e391b9862a70a09632bd0891341397d1b
vipuliitm <118936812+vipuliitm@users.noreply.github.com>
2022-11-24 03:48:26 +0530

    Update tds.py

--- tds.py
+++ tds.py
@@ -1,10 +1,9 @@
 !pip install streamlit
 import streamlit as st
-import numpy as np
 
 st.header("Multiplication of Two Numbers")
 a = st.number_input("Input First Number")
 b = st.number_input("Input Second Number")
-c = np.multiply(a,b)
+c = a*b
 st.subheader("Multiplication of two numbers is")
 st.write(c)
