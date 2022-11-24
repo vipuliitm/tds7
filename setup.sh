ad2550c08dad7867bb406dc8a7417b1c14b50d6b
vipuliitm <118936812+vipuliitm@users.noreply.github.com>
2022-11-24 23:08:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/  
+
 echo "\
 [general]\n\
-email = \"21f1005862@student.onlinedegree.iitm.ac.in"\n\
-"> ~/.streamlit/credntials.toml
+email = \"21f1005862@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless= true\n\
 enableCORS=false\n\
 port = $PORT\n\
-"> ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
